14a2f92f0b549722f40bf0fb92ad9ed52339a3a4
aFaneca <me@afaneca.com>
2017-11-11 00:10:30 +0000

    Update Servidor.c

--- Servidor.c
+++ Servidor.c
@@ -6,16 +6,16 @@
 
 #define NMAXPLAY 20
 
-typedef struct Labirinto{
+typedef struct Labirinto {
 	char paredes[20][30];
 } labirinto;
 
-typedef struct Login{
+typedef struct Login {
 	char username[100];
 	char password[100];
 } login;
 
-typedef struct Cliente{
+typedef struct Cliente {
 	login l;
 	bool online;
 	double pontuacao;
@@ -24,64 +24,72 @@ typedef struct Cliente{
 } cliente;
 
 
-char ** processaComando(char *comando, int *tamCMD){
+char ** processaComando(char *comando, int *tamCMD) {
 
-	char ** cmd  = NULL;
-	char *  p    = strtok (comando, " ");
+	char ** cmd = NULL;
+	char *  p = strtok(comando, " ");
 	int n_espacos = 0, i;
 
 
 	while (p) { 	/* divide a string em palavras */
-	 cmd = realloc (cmd, sizeof (char*) * ++n_espacos);
+		cmd = realloc(cmd, sizeof(char*) * ++n_espacos);
 
-	  if (cmd == NULL)
-		exit (-1); /* se alocação de memória falhar */
+		if (cmd == NULL)
+			exit(-1); /* se alocação de memória falhar */
 
-	  cmd[n_espacos-1] = p;
-	  p = strtok (NULL, " ");
+		cmd[n_espacos - 1] = p;
+		p = strtok(NULL, " ");
 	}
 
 	/* realoca um elemento extra para o "NULL" final */
-	cmd = realloc (cmd, sizeof (char*) * (n_espacos+1));
+	cmd = realloc(cmd, sizeof(char*) * (n_espacos + 1));
 	cmd[n_espacos] = 0;
 
 	*tamCMD = n_espacos;
 	return cmd;
-  }
-
-
-login* add(login* v,char *cmd[],int *conta){
-
-		int i = 0;
-		int flag = 0;
-		for(i = 0; i < *conta; i++){
-			if(strcmp(cmd[1], v[i].username) == 0){
-				printf("Username existente!!\n");
-				flag = 1;
-			}
-		}
-
-		if(flag == 0){
-			*conta++;
-    			login *temp = realloc(v,sizeof(login)*(*conta));
-			printf("Adicionado User Nº%d --> %s\n",*conta, cmd[1]);
-			strcpy(temp[*conta-1].username, cmd[1]);
-        		strcpy(temp[*conta-1].password, cmd[2]);
-			return temp;
-		}else{
-			return v;
-		}
 }
 
-int contaLinhas(){
 
-	int conta=0;
+login* add(login* v, char *cmd[], int *conta) {
+
+	int i = 0;
+	int flag = 0;
+
+	for (i = 0; i < *conta; i++) {
+		if (strcmp(cmd[1], v[i].username) == 0) {
+			printf("Username existente!!\n");
+			flag = 1;
+		}
+	}
+	if (flag == 0) {
+		*conta = *conta + 1;
+		login *temp = realloc(v, sizeof(login)*(*conta));
+		printf("Adicionado User No. %d --> %s\n", *conta, cmd[1]);
+		strcpy(temp[*conta - 1].username, cmd[1]);
+		strcpy(temp[*conta - 1].password, cmd[2]);
+		return temp;
+	}
+	else {
+		return v;
+	}
+
+
+
+	return v;
+}
+
+int contaLinhas() {
+
+	int conta = 0;
 	char linha[256];
-	FILE *f = fopen("logs.txt","r");
-
-	while(fgets(linha, sizeof(linha), f) != NULL){
-                        conta++;
-        }
+	FILE *f = fopen("logs.txt", "r");
+	if (f == NULL) {
+		printf("Erro a abrir ficheiro 1.");
+		return 1;
+	}
+	while (fgets(linha, sizeof(linha), f) != NULL) {
+		conta++;
+	}
 
 	conta = conta / 2;
 	printf("Pessoas a jogar = %d\n", conta);
@@ -90,96 +98,105 @@ int contaLinhas(){
 	return conta;
 }
 
-void buscaLogs(login *v){
+void buscaLogs(login *v) {
 
-    int k = 0;
-    FILE *f = fopen("logs.txt","r");
-
-        while(!feof(f)){
-            	fscanf(f, "%s", v[k].username);
-            	fscanf(f, "%s", v[k].password);
+	int k = 0;
+	FILE *f = fopen("logs.txt", "r");
+	if (f == NULL) {
+		printf( "Erro a abrir ficheiro2.");
+		return 1;
+	}
+	while (!feof(f)) {
+		fscanf(f, "%s", v[k].username);
+		fscanf(f, "%s", v[k].password);
 		k++;
-        }
-    fclose(f);
+	}
+	fclose(f);
 }
 
-void users(login *v, int conta){
+void users(login *v, int conta) {
 
-    int k = 0;
-    if(conta == 0){
-        printf("Nao existem Users a jogar\n");
-    }
-	for(k=0;k < conta;k++){
-            printf("%s\n", v[k].username);
-        }
+	int k = 0;
+	if (conta == 0) {
+		printf("Nao existem Users a jogar\n");
+	}
+	for (k = 0; k < conta; k++) {
+		printf("%s\n", v[k].username);
+	}
 }
 
 
-void main(int argc, char *argv[]){
+void main(int argc, char *argv[]) {
 	char *comando = NULL;
-	comando = malloc(sizeof(char)*20);
+	comando = malloc(sizeof(char) * 20);
 	char **cmd;
-	int *tamCMD, i;
+	int tamCMD = 0, i;
 
 	login *v;
 	int total = contaLinhas();
-    	v = malloc(sizeof(login)*total);
+	v = malloc(sizeof(login)*total);
 	buscaLogs(v);
 
 
-	while(1){
+	while (1) {
+		if (stdin == NULL) {
+			printf("Erro a abrir ficheiro3.");
+			return 1;
+		}
 		fgets(comando, 25, stdin);
 
-		cmd = processaComando(comando, tamCMD);
+		cmd = processaComando(comando, &tamCMD);
+		//printf("%s %d", cmd[0], tamCMD);
 
-		if(strncmp(cmd[0], "add", 3)==0){
-			if(*tamCMD == 3){
-				//v = add(v, cmd, total);
-				//users(v, total);
+		if (strncmp(cmd[0], "add", 3) == 0) {
+			if (tamCMD == 3) {
+				v = add(v, cmd, &total);
+				users(v, total);
 
-			}else{
+			}
+			else {
 				printf("Sintaxe ERROR");
 			}
 		}
 
-		if(strncmp(cmd[0], "game", 4) == 0){
-			if(*tamCMD != 3)
+		if (strncmp(cmd[0], "game", 4) == 0) {
+			if (tamCMD != 3)
 				printf("Erro de Sintaxe.");
 			else
 				printf("Username: %s", cmd[1]);
 		}
 
-		if(strncmp(cmd[0], "users", 5) == 0){
-			if(*tamCMD == 1)
+		if (strncmp(cmd[0], "users", 5) == 0) {
+			if (tamCMD == 1)
 				users(v, total);
 			else
 				printf("Erro de Sintaxe.");
 		}
 
-		if(strncmp(cmd[0], "kick", 4) == 0){
-			if(*tamCMD != 2)
+		if (strncmp(cmd[0], "kick", 4) == 0) {
+			if (tamCMD != 2)
 				printf("Erro de Sintaxe. kick <username>");
 			else
 				printf("User kickado: ...");
 		}
 
-		if(strncmp(cmd[0], "map", 3) == 0){
-			if(*tamCMD != 2)
+		if (strncmp(cmd[0], "map", 3) == 0) {
+			if (tamCMD != 2)
 				printf("Erro de Sintaxe. kick <nome-ficheiro>");
 			else
 				printf("Mapa Carregado ...");
 		}
-		if(strncmp(cmd[0], "game", 4) == 0){
-			if(*tamCMD != 1)
+		if (strncmp(cmd[0], "game", 4) == 0) {
+			if (tamCMD != 1)
 				printf("Erro de Sintaxe.");
 			else
 				printf("Game: ...");
 		}
-		if(strncmp(cmd[0], "shutdown", 8) == 0){
-            if(*tamCMD != 1 || strlen(cmd[0]) != 9)
+		if (strncmp(cmd[0], "shutdown", 8) == 0) {
+			if (tamCMD != 1 || strlen(cmd[0]) != 9)
 				printf("Erro de Sintaxe.");
 			else
 				break;
-        }
+		}
 	}
 }
