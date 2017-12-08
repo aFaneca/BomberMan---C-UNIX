855244f354eeea65d1305be1e954650ca5262371
GentleBee <killlo325@hotmail.com>
2017-12-08 05:02:01 +0000

    Update - logout melhorado e login protegido

---
+++ Projeto/TPSO/TPSO/processaCliente.c
@@ -0,0 +1,61 @@
+#include "cliente.h"
+
+
+int login(){
+	
+	char user[50], pass[50];
+	
+	printf("\nUser: ");
+    fgets(user, 50, stdin);
+    user[strlen(user)-1]='\0';
+
+    printf("Password: ");
+    fgets(pass, 50, stdin);
+    pass[strlen(pass)-1]='\0';
+
+    strcpy(msg.op1, "login");
+    strcpy(msg.op2, user);
+    strcpy(msg.op3, pass);
+	
+    write(fd_servidor, &msg, sizeof(msg)); 		// ENVIAR PEDIDO PARA "CP" DO SERVIDOR (write)
+    fd_cliente = open(msg.endereco, O_RDONLY); 	// ABRIR "CP" DO CLIENTE (open - O_RDONLY)
+	read(fd_cliente, &msg, sizeof(msg));		// RECEBER RESPOSTA NA "CP" DO CLIENTE (read)
+    close(fd_cliente); 							// FECHAR "CP" DO CLIENTE - MINHA (close)
+    printf("%s\n", msg.resposta);     			//FAZER RECEBIMENTO
+	if(strcmp(msg.op4, "n") == 0)
+		return 0;
+	else
+		return 1;
+	
+}
+
+char ** processaComando(char *comando, int *tamCMD) {
+
+	char ** cmd = NULL;
+	char *  p = strtok(comando, " ");
+	int n_espacos = 0;
+	int i = 0;
+
+
+	while (p) { 	/* divide a string em palavras */
+		cmd = realloc(cmd, sizeof(char*) * ++n_espacos);
+
+		if (cmd == NULL)
+			exit(-1); /* se alocação de memória falhar */
+
+		cmd[n_espacos - 1] = p;
+		p = strtok(NULL, " ");
+	}
+
+	char lastWord[25];
+	char lastWord2[25];
+	strcpy(lastWord, cmd[n_espacos -1]);
+
+	for(i=0;i < strlen(lastWord) - 1; i++){
+		lastWord2[i] = lastWord[i];
+	}
+	strcpy(cmd[n_espacos - 1], lastWord2);
+	*tamCMD = n_espacos;
+
+	return cmd;
+}
